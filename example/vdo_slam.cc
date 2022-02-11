b2df60e7676f800acd1f4f4153512d668aa04899
WFram <earl.freedom.ea@gmail.com>
2022-02-11 12:53:00 +0300

    Test commit

--- example/vdo_slam.cc
+++ example/vdo_slam.cc
@@ -115,14 +115,12 @@ int main(int argc, char **argv)
         // imD_r.convertTo(imD_f, CV_32F);
 
         // Load flow matrix
-        // cv::Mat imFlow = cv::optflow::readOpticalFlow(vstrFilenamesFLO[ni]);
-        cv::Mat imFlow = readOpticalFlow(vstrFilenamesFLO[ni]);
-        // FlowShow(imFlow);
-
+        // imFlow = readOpticalFlow(vstrFilenamesFLO[ni]);
+        // imRGB.copyTo(imRGB_prev);
         // Load semantic mask
         cv::Mat imSem(imRGB.rows, imRGB.cols, CV_32SC1);
         LoadMask(vstrFilenamesSEM[ni],imSem);
-
+        //
         double tframe = vTimestamps[ni];
         mTcw_gt = vPoseGT[ni];
 
@@ -137,9 +135,12 @@ int main(int argc, char **argv)
             return 1;
         }
 
+        cv::Mat imRGB_prev;
         // Pass the image to the SLAM system
-        SLAM.TrackRGBD(imRGB,imD_f,imFlow,imSem,mTcw_gt,vObjPose_gt,tframe,imTraj,nImages);
-
+        if (ni != 0) {
+            cv::Mat imFlow = denseOpticalFlow(imRGB, imRGB_prev);
+            SLAM.TrackRGBD(imRGB,imD_f,imFlow,imSem,mTcw_gt,vObjPose_gt,tframe,imTraj,nImages);
+        }
     }
 
     // Save camera trajectory
